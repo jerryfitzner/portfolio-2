f1086bc720fe8f55ac7e4e7cb8efc0cd3b5bd21d
Jerry Fitzner <jfitzner1@gmail.com>
2023-08-01 16:27:36 -0400

    Added links to buttons

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SFPic from '../assets/SF.coverimage.png';
 import ICAPic from '../assets/ICA.coverimage.png';
 
@@ -7,38 +7,42 @@ const Projects = () => {
 
   const projects = [
     {
-      id: 1,
+      id: 0,
       name: 'Incident Command App',
       imgsrc: ICAPic,
       alt: 'Incident Command Project Photo',
-      code: '',
-      demo: ''
+      code: 'https://github.com/jerryfitzner/incident-command',
+      demo: 'https://youtu.be/A_xAV2Jx0zs'
     },
     {
-      id: 2,
+      id: 1,
       name: 'Student Fundraising App',
       imgsrc: SFPic,
       alt: 'Student Fundraising Project Photo',
-      code: '',
-      demo: ''
+      code: 'https://github.com/jerryfitzner/phase-3-project-frontend',
+      demo: 'https://youtu.be/kVg8fz3EWW8'
     },
   ]
 
   // const num 
 
+  const linkClick = (link) => {
+    window.open(link, '_blank', 'noreferrer')
+  }
+
   return (
     <div name='projects' className='w-full md:h-screen'>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Projects</p>
         </div>
         <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-8 px-12 sm:px-0'>
         {projects.map(({id, name, alt, imgsrc, code, demo}) => (
-          <div className='shadow-md shadow-gray-600 rounded-lg'>
+          <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
             <img src={imgsrc} alt={alt} className='rounded-md duration-200 hover:scale-105'/>
             <div className='flex items-center justify-center'>
-              <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-              <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+              <button onClick={() => linkClick(demo)} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+              <button onClick={() => linkClick(code)} href={code} className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
             </div>
           </div>
         ))}
